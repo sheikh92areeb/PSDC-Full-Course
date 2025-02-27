54cde87870366552f92eccd1c5f49d847e6afb53
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-28 00:09:29 +0500

    Create index.js

---
+++ assignments/assignment-17/index.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+
+    return arr;
+}
+
+let array = [3,9,1,7,5];
+console.log(bubbleSort(array)); 
