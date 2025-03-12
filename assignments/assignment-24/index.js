d1ebc30dd459c196f432027e62e271795cbd4194
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-12 14:23:17 +0500

    Create index.js

---
+++ assignments/assignment-24/index.js
@@ -0,0 +1,12 @@
+const cars = { make:"Toyota", model:"Camry"}
+
+let {make: make, model: model} = cars
+
+console.log(make, model)
+
+
+const students = ["Ahmad", 20, "Karachi"]
+
+let [student_name, age, city] = students
+
+console.log(student_name , age, city)
