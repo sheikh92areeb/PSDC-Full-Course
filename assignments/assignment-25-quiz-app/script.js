7899046f3167b5a1f4d5158336f775554a199326
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-03-14 19:54:15 +0500

    update

--- assignments/assignment-25-quiz-app/script.js
+++ assignments/assignment-25-quiz-app/script.js
@@ -6,9 +6,11 @@ let username = "";
 // Fetch questions from JSON Server
 async function fetchQuestions() {
     try {
+        console.log("Fetching Questions")
         const res = await fetch("http://localhost:3000/questions");
         questions = await res.json();
         showQuestion();
+        console.log("Calling Show Question")
     } catch (error) {
         console.error("Error fetching questions:", error);
     }
@@ -16,6 +18,7 @@ async function fetchQuestions() {
 
 // Start quiz
 function startQuiz() {
+    console.log("Start Quiz ...")
     username = document.getElementById("username").value;
     if (!username) {
         alert("Please enter your name!");
@@ -23,11 +26,13 @@ function startQuiz() {
     }
     document.getElementById("user-info").classList.add("hidden");
     document.getElementById("quiz-box").classList.remove("hidden");
+    console.log("Calling Fetching Question")
     fetchQuestions();
 }
 
 // Show question
 function showQuestion() {
+    console.log("Showing Question")
     if (currentQuestionIndex >= questions.length) {
         return endQuiz();
     }
@@ -42,8 +47,9 @@ function showQuestion() {
         const btn = document.createElement("button");
         btn.innerText = option;
         btn.onclick = () => {
+            console.log("Option Clicked")
+            console.log("Calling Check Answer")
             checkAnswer(index)
-            btn.style.backgroundColor = "#696996"
         };
         optionsContainer.appendChild(btn);
     });
@@ -51,6 +57,7 @@ function showQuestion() {
 
 // Check answer
 function checkAnswer(selectedIndex) {
+    console.log("Checking Answer")
     const questionData = questions[currentQuestionIndex];
 
     if (selectedIndex === questionData.correct) {
@@ -65,16 +72,20 @@ function checkAnswer(selectedIndex) {
 
 // Next question
 function nextQuestion() {
+    console.log("Next Questions")
     currentQuestionIndex++;
+    console.log("Calling Show Questions")
     showQuestion();
 }
 
 // End quiz & save user score
 async function endQuiz() {
+    console.log("Ending Quiz")
     document.getElementById("quiz-box").classList.add("hidden");
     document.getElementById("result-box").classList.remove("hidden");
     document.getElementById("result-text").innerText = `Congrats ${username}! Your score is: ${score}`;
 
+    console.log("Post Request")
     try {
         await fetch("http://localhost:3000/users", {
             method: "POST",
@@ -88,5 +99,6 @@ async function endQuiz() {
 
 // Restart quiz
 function restartQuiz() {
+    console.log("Resart Quiz")
     location.reload();
 }
